5dd12e8333820e4839f2b4f0278d3b1532e9a6a6
JavohirJalilov <javohirjaliloff@gmail.com>
2023-06-08 10:27:59 +0500

    fix

--- logical_operators/tests.py
+++ logical_operators/tests.py
@@ -2,7 +2,7 @@ from logical_operators import TaskOne
 
 q1 = TaskOne("task_1", "homework_1")
 
-def solution(a, b, c):
-    return a < b < c
+# def solution(a, b, c):
+#     return a < b < c
 
-q1.check(solution, "username")
+# q1.check(solution, "username")
